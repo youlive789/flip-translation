abff346fe480fd25899106538b05a686a03a4e74
gwkim <youlive789@gmail.com>
2020-09-07 16:07:58 +0900

    feat: Add data class to flip-translation

---
+++ data.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+"""[reference]
+https://colab.research.google.com/github/tensorflow/docs/blob/master/site/en/tutorials/text/nmt_with_attention.ipynb?hl=ko#scrollTo=rd0jw-eC3jEh
+"""
+
+class Dataset:
+    def __init__(self, data_dir):
+        pass
+
