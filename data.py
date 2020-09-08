7abe3c4e5e6d1a3e05ba4b8a81148f022fbb0d9f
gwkim <youlive789@gmail.com>
2020-09-08 18:42:24 +0900

    feat: Modify data class => sentence preprocessing

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
+import re
+# import tensorflow as tf
 
 """[reference]
 https://colab.research.google.com/github/tensorflow/docs/blob/master/site/en/tutorials/text/nmt_with_attention.ipynb?hl=ko#scrollTo=rd0jw-eC3jEh
@@ -6,7 +7,27 @@ https://colab.research.google.com/github/tensorflow/docs/blob/master/site/en/tut
 
 class Dataset:
     def __init__(self, dir_source, dir_target):
+        self.dir_source = dir_source
+        self.dir_target = dir_target
+
+    def _read_files():
         pass
 
-    def _read_files()
-        pass
+    def _preprocess_sentence(self, sentence):
+        # 구두점처리
+        sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
+        sentence = re.sub(r'[" "]+', " ", sentence)
+        sentence = sentence.strip()
+        sentence = "<start> " + sentence + " <end>"
+        return sentence
+
+if __name__ == "__main__":
+    with open("data/train.ko", encoding="utf-8") as f:
+        ko = f.readlines()
+
+    with open("data/train.en", encoding="utf-8") as f:
+        en = f.readlines()
+
+    dataset = Dataset("s", "t")
+    print(ko[0])
+    print(dataset._preprocess_sentence(ko[0]))
